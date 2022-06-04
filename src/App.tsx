9a6e778ea968bda0ded9fd6c23662fbffe3dd158
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-06-04 17:26:15 +0900

    Ass piechart

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Cards from "./features/covid/Cards/Cards";
 import Chart from "./features/covid/Chart/Chart";
+import PieChart from "./features/covid/PieChart/PieChart";
 
 function App() {
   return (
     <div className="App">
       <Cards />
       <Chart />
+      <PieChart />
     </div>
   );
 }
